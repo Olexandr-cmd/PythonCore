1c31dd093d7b446c7df164510614cd5fe683e5ea
admin <disaster1209@ukr.net>
2021-02-23 14:45:33 +0200

    lesson1

--- lesson1.py
+++ lesson1.py
@@ -1,6 +1,6 @@
 # 3) переделать первое задание под меню с помощью цикла
 # Дан лист:
-# my_list = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
+# my_list = [1, 2, 2, 3, 4, 2, 2, 4, 2, 4, 4, 4, 2]
 #
 # while True:
 #     print('1.найти min число в листе')
@@ -15,10 +15,7 @@
 #         print('min_num', my_list[0])
 #
 #     elif choice == '2':
-#         for num in my_list:
-#             if my_list.count(num) > 1:
-#                 my_list.remove(num)
-#         for num in my_list:
+#         for num in my_list[::-1]:
 #             if my_list.count(num) > 1:
 #                 my_list.remove(num)
 #         print(my_list)
@@ -81,4 +78,62 @@
 #         j += 1
 #     print()
 #     i += 1
-
+# реализовать записную книжку покупок:
+# - каждая запись должна содержать название покупки и ее цену
+# -сделать менюшку для работы с записной книжкой:
+# costs = [{'date': '23.02', 'name': 'milk', 'price': 20},
+#          {'date': '20.02', 'name': 'bread', 'price': 25}]
+#
+# while True:
+#     print('1) Создать запись')
+#     print('2) Список все записей')
+#     print('3) Общая сумма всех покупок')
+#     print('4) Самая дорогая покупка')
+#     print('5) Поиск по названию покупки')
+#     print('6) Поиск по дате')
+#     print('7) Выход')
+#
+#     choice = input('Зробіть свій вибір')
+#
+#     if choice == '1':
+#         date = input('Введіть дату (XX:XX)')
+#         name = input('Введіть назву')
+#         price = int(input('Ціна :'))
+#
+#         costs.append({'date': date, 'name': name, price: price})
+#
+#     elif choice == '2':
+#         print(costs)
+#
+#     elif choice == '3':
+#         spending = 0
+#         for cost in costs:
+#             spending += cost.get('price')
+#         print(spending)
+#
+#     elif choice == '4':
+#         maxPrice = {'date': '12.05', 'name': 'test', 'price': 0}
+#         for cost in costs:
+#             if cost['price'] > maxPrice['price']:
+#                 maxPrice.update(cost)
+#         print(maxPrice)
+#
+#     elif choice == '5':
+#         findName = input('Введіть назву товару')
+#
+#         for cost in costs:
+#             if cost['name'] == findName:
+#                 print(cost)
+#
+#     elif choice == '6':
+#         findDate = input('Введіть дату (ХХ:ХХ)')
+#
+#         for cost in costs:
+#             if cost['date'] == findDate:
+#                 print(cost)
+#
+#     elif choice == '7':
+#         break
+#
+#     else:
+#         print('Введіть число із списку')
